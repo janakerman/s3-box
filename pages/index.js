3d47ce7c82ffd3b1ad727ad9a8818dccf0978eac
Jan Akerman <jan.akerman@scottlogic.co.uk>
2018-05-02 14:02:33 +0100

    Adds express server

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-const Index = () => (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-)
+import { Component } from 'react'
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello Next.js</p>
+            </div>
+        )
+    }
+}
   
 export default Index
